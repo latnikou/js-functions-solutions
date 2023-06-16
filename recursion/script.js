85efcd34f52255d1199f5a49cd58e931cfba3586
Maxim Latnikov <latnikou@gmail.com>
2023-06-16 14:59:27 +0300

    solved recursion task

---
+++ recursion/script.js
@@ -0,0 +1,15 @@
+// @ts-check
+
+const sequenceSum = (begin, end) => {
+  // BEGIN (write your solution here)
+  if (begin > end) {
+    return NaN;
+  }
+  if (begin === end) {
+    return begin;
+  }
+  return begin + sequenceSum(begin + 1, end);
+  // END
+};
+
+export default sequenceSum;
